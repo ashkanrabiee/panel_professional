08026556b70c7650ab3bd62cefc6233f9ab603c9
yacn114 <yacn1214@gmail.com>
2025-03-27 16:02:03 +0330

    product form

--- resources/views/panel/content/post/create.blade.php
+++ resources/views/panel/content/post/create.blade.php
@@ -6,9 +6,32 @@
 
 
 @section('content')
+<style>
+    .custom-form {
+        background: #f8f9fa; /* رنگ پس‌زمینه صفحه */
+    }
+    
+    .form-container {
+        width: 100%;
+        background: #ffffff;
+    }
+    
+    .form-label {
+        margin-bottom: 5px;
+        color: #333;
+        display: block; /* تضمین می‌کنه لیبل به صورت بلوکی نمایش داده شود */
+    }
+    
+    .form-control {
+        border-radius: 8px; /* گوشه‌های گرد */
+        border: 1px solid #ccc;
+        padding: 10px;
+    }
+    </style>
+    
 <div class="intro-y flex items-center mt-8">
     <h2 class="text-lg font-medium mr-auto">
-        Form Layout
+        ساخت محصول
     </h2>
 </div>
 <div class="flex justify-center items-center min-h-screen">
@@ -16,75 +39,101 @@
         <div class="intro-y col-span-12">
             <!-- BEGIN: Form Layout -->
             <div class="intro-y box p-5">
+<form action="" method="post">
+    @csrf
                 <div>
-                    <label>Product Name</label>
-                    <input type="text" class="input w-full border mt-2" placeholder="Input text">
+                    <label>ساخت محصول</label>
+                    <input type="text" name="name" class="input w-full border mt-2" placeholder="نام محصول">
                 </div>
                 <div class="mt-3">
-                    <label>Category</label>
+                    <label>مقدمه</label>
                     <div class="mt-2">
-                        <select data-placeholder="Select your favorite actors" class="select2 w-full" multiple>
-                            <option value="1" selected>Sport & Outdoor</option>
-                            <option value="2">PC & Laptop</option>
-                            <option value="3" selected>Smartphone & Tablet</option>
-                            <option value="4">Photography</option>
-                        </select>
+                        <textarea data-feature="basic" name="introduction" class="summernote"></textarea>
                     </div>
                 </div>
                 <div class="mt-3">
-                    <label>Quantity</label>
-                    <div class="relative mt-2">
-                        <input type="text" class="input pr-12 w-full border col-span-4" placeholder="Price">
-                        <div class="absolute top-0 right-0 rounded-r w-10 h-full flex items-center justify-center bg-gray-100 border text-gray-600">pcs</div>
-                    </div>
+                <div>
+                    <label>اسلاگ</label>
+                    <input type="text" name="name" class="input w-full border mt-2" placeholder="اسلاگ">
                 </div>
-                <div class="mt-3">
-                    <label>Weight</label>
-                    <div class="relative mt-2">
-                        <input type="text" class="input pr-16 w-full border col-span-4" placeholder="Price">
-                        <div class="absolute top-0 right-0 rounded-r w-16 h-full flex items-center justify-center bg-gray-100 border text-gray-600">grams</div>
-                    </div>
                 </div>
-                <div class="mt-3">
-                    <label>Price</label>
-                    <div class="sm:grid grid-cols-3 gap-2">
-                        <div class="relative mt-2">
-                            <div class="absolute top-0 left-0 rounded-l w-12 h-full flex items-center justify-center bg-gray-100 border text-gray-600">Unit</div>
-                            <div class="pl-3">
-                                <input type="text" class="input pl-12 w-full border col-span-4" placeholder="Price">
-                            </div>
+                    <div class="mt-3">
+                        <div>
+                            <label>وزن</label>
+                            <input type="number" name="weight" class="input w-full border mt-2" placeholder="وزن">
                         </div>
-                        <div class="relative mt-2">
-                            <div class="absolute top-0 left-0 rounded-l w-20 h-full flex items-center justify-center bg-gray-100 border text-gray-600">Wholesale</div>
-                            <div class="pl-3">
-                                <input type="text" class="input pl-20 w-full border col-span-4" placeholder="Price">
-                            </div>
                         </div>
-                        <div class="relative mt-2">
-                            <div class="absolute top-0 left-0 rounded-l w-12 h-full flex items-center justify-center bg-gray-100 border text-gray-600">Bulk</div>
-                            <div class="pl-3">
-                                <input type="text" class="input pl-12 w-full border col-span-4" placeholder="Price">
+                        <div class="mt-3">
+                            <div>
+                                <label>طول</label>
+                                <input type="number" name="length" class="input w-full border mt-2" placeholder="طول">
                             </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="mt-3">
-                    <label>Active Status</label>
-                    <div class="mt-2">
-                        <input type="checkbox" class="input input--switch border">
-                    </div>
-                </div>
-                <div class="mt-3">
-                    <label>Textarea</label>
-                    <div class="mt-2">
-                        <textarea data-feature="basic" class="summernote" name="editor"></textarea>
-                    </div>
-                </div>
+                            </div>
+                            <div class="mt-3">
+                                <div>
+                                    <label>عرض</label>
+                                    <input type="number" name="width" class="input w-full border mt-2" placeholder="عرض">
+                                </div>
+                                </div>
+                                <div class="mt-3">
+                                    <div>
+                                        <label>ارتفاع</label>
+                                        <input type="number" name="height" class="input w-full border mt-2" placeholder="ارتفاع">
+                                    </div>
+                                    </div>
+                                    <div class="mt-3">
+                                        <div>
+                                            <label>قیمت</label>
+                                            <input type="number" name="price" class="input w-full border mt-2" placeholder="قیمت">
+                                        </div>
+                                        </div>
+                                        <div class="mt-3">
+                                            <label>وضعیت</label>
+                                            <div class="mt-2">
+                                                <input type="checkbox" class="input input--switch border" name="status">
+                                            </div>
+                                        </div>
+                                        <div class="mt-3">
+                                            <label>اجازه فروش</label>
+                                            <div class="mt-2">
+                                                <input type="checkbox" class="input input--switch border" name="marketable">
+                                            </div>
+                                        </div>
+                                        <div class="mt-3">
+                                            <div>
+                                                <label>تگ ها</label>
+                                                <input type="text" name="tags" class="input w-full border mt-2" placeholder="اسلاگ">
+                                            </div>
+                                            </div>
+                                            <div class="custom-form container-fluid d-flex justify-content-center align-items-center min-vh-100">
+                                                <div class="form-container p-4 border rounded shadow-sm bg-white" style="max-width:800px;">
+                                                    <div class="row">
+                                                        <div class="col-md-6">
+                                                            <div class="form-group mb-3">
+                                                                <label class="form-label fw-bold d-block">برندها</label>
+                                                                <select name="brands" class="form-control form-select mt-1">
+                                                                    <option value="" selected="">لطفا انتخاب کن</option>
+                                                                </select>
+                                                            </div>
+                                                        </div>
+                                                        <div class="col-md-6">
+                                                            <div class="form-group mb-3">
+                                                                <label class="form-label fw-bold d-block">کتگوری ها</label>
+                                                                <select name="categories" class="form-control form-select mt-1">
+                                                                    <option value="" selected="">لطفا انتخاب کن</option>
+                                                                </select>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            
+
                 <div class="text-right mt-5">
-                    <button type="button" class="button w-24 border text-gray-700 mr-1">Cancel</button>
-                    <button type="button" class="button w-24 bg-theme-1 text-white">Save</button>
+                    <button type="submit" class="button w-24 bg-theme-1 text-white">Save</button>
                 </div>
             </div>
+        </form>
             <!-- END: Form Layout -->
         </div>
     </div>
