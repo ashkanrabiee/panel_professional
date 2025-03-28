bb1782a80c32a95ffa954eb0a8e8741230b65fe5
yacn114 <yacn1214@gmail.com>
2025-03-28 22:13:53 +0330

    end category content section and post

--- routes/web.php
+++ routes/web.php
@@ -198,9 +198,9 @@ Route::prefix('admin')->group(function(){
                   Route::get('/',[ContentCategoryController::class , 'index'])->name('admin.content.category.index');
                   Route::get('/create',[ContentCategoryController::class , 'create'])->name('admin.content.category.create');
                   Route::post('/store',[ContentCategoryController::class , 'store'])->name('admin.content.category.store');
-                  Route::get('/edit',[ContentCategoryController::class , 'edit'])->name('admin.content.category.edit');
+                  Route::get('/edit/{postCategory}',[ContentCategoryController::class , 'edit'])->name('admin.content.category.edit');
                   Route::patch('/update/{postCategory}',[ContentCategoryController::class , 'update'])->name('admin.content.category.update');
-                  Route::delete('/destroy/{postCategory}',[ContentCategoryController::class , 'edit'])->name('admin.content.category.destroy');
+                  Route::delete('/destroy/{PostCategory}', [ContentCategoryController::class, 'destroy'])->name('admin.content.category.destroy');
                   Route::get('/status/{postCategory}', [ContentCategoryController::class, 'status'])->name('admin.content.category.status');
             });
 
